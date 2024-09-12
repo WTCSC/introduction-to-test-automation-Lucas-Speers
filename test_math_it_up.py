5bc995027612e0bb53aae7a601d77d2fa8e7d25f
Lucas Speers <lucas_speers@outlook.com>
2024-09-12 10:20:34 -0600

    added tests

--- test_math_it_up.py
+++ test_math_it_up.py
@@ -27,27 +27,51 @@ To run the tests, run `pytest` from the command line in the same directory as
 this file.
 """
 
+@pytest.fixture
+def sample_data():
+  """
+  Sample
+  """
+  return [6, -42, 86, 0, 56, 86]
+
 def test_is_even():
   """
   Tests for the `is_even` function
   """
+  assert math_it_up.is_even(0)
+  assert math_it_up.is_even(6)
+  assert math_it_up.is_even(-42)
+
+  assert not math_it_up.is_even(1)
+  assert not math_it_up.is_even(9)
+  assert not math_it_up.is_even(-71)
 
 def test_is_odd():
   """
   Tests for the `is_odd` function
   """
+  assert not math_it_up.is_odd(0)
+  assert not math_it_up.is_odd(6)
+  assert not math_it_up.is_odd(-42)
 
-def test_mean():
+  assert math_it_up.is_odd(1)
+  assert math_it_up.is_odd(9)
+  assert math_it_up.is_odd(-71)
+
+def test_mean(sample_data):
   """
   Tests for the `mean` function
   """
+  assert math_it_up.mean(sample_data) == 32
 
-def test_median():
+def test_median(sample_data):
   """
   Tests for the `median` function
   """
+  assert math_it_up.median(sample_data) == 31
 
-def test_mode():
+def test_mode(sample_data):
   """
   Tests for the `mode` function
   """
+  assert 86 in math_it_up.mode(sample_data)
